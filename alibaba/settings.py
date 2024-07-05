3b4109dd84e3809f2cb48754d79652bb4432bd9a
younes <apotheek67@gmail.com>
2024-07-05 02:18:18 +0200

    add fa-ir/Asia/Tehran -django

--- alibaba/settings.py
+++ alibaba/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p(r&z08n$exoy2p(ms28x0$cjgm8xo^ilburb8i$rlo0y(q9b9'
+SECRET_KEY = 'django-insecure-ym_lo^n2@nmbpi96tl1as0l-^r*7-=1byxv84s_i^s!)3i=cqw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ticketsales',
+     'ticketsales',
 ]
 
 MIDDLEWARE = [
@@ -105,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa-ir'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tehran'
 
 USE_I18N = True
 
@@ -116,7 +116,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = 'static/'
 STATIC_ROOT=os.path.join(BASE_DIR,"static")
 # Default primary key field type
--- ticketsales/models.py
+++ ticketsales/models.py
@@ -5,13 +5,17 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class login_user(models.Model):                                                  
+class login_user(models.Model):    
+    class Meta: 
+        verbose_name="ورود کاربر "  
+        verbose_name_plural="ورود کاربر" 
+                                                  
     ID_user = models.ForeignKey(User,on_delete=models.CASCADE)
-    First_name = models.CharField(max_length=100,null=False,blank=False)
-    Last_name = models.CharField(max_length=100,null=False,blank=False)
-    Phone_number=models.CharField(max_length=11,null=False,blank=False)
-    Email= models.EmailField(max_length=254,null=False,blank=False)
-    Passwoord=models.CharField(max_length=50)
+    First_name = models.CharField(max_length=100,null=False,blank=False,verbose_name="نام ")
+    Last_name = models.CharField(max_length=100,null=False,blank=False,verbose_name="نام خانوادگی ")
+    Phone_number=models.CharField(max_length=11,null=False,blank=False,verbose_name="تلفن ")
+    Email= models.EmailField(max_length=254,null=False,blank=False,verbose_name="ایمیل ")
+    Passwoord=models.CharField(max_length=50,verbose_name="پسورد ")
     Register_date= models.DateTimeField("date published")
 
     def __str__(self):
@@ -20,13 +24,17 @@ class login_user(models.Model):
  ###################################### login_Admin ##############################################
 
 class login_Admin(models.Model): 
+    class Meta: 
+        verbose_name="ورود ادمین "  
+        verbose_name_plural="ورود ادمین" 
+
     ID_Admin = models.ForeignKey(User,on_delete=models.CASCADE)
-    First_name_Admin = models.CharField(max_length=100,null=False,blank=False)
-    Last_name_Admin = models.CharField(max_length=100,null=False,blank=False)
-    Phone_number_Admin=models.CharField(max_length=11,null=False,blank=False)
-    Email_user_Admin= models.EmailField(max_length=100,null=False,blank=False)
-    Passwoord_user_Admin=models.CharField(max_length=50)
-    Register_date_Admin= models.DateTimeField("date published")
+    First_name_Admin = models.CharField(max_length=100,null=False,blank=False,verbose_name="نام ")
+    Last_name_Admin = models.CharField(max_length=100,null=False,blank=False,verbose_name=" نام خانوادگی ")
+    Phone_number_Admin=models.CharField(max_length=11,null=False,blank=False,verbose_name="تلفن ")
+    Email_user_Admin= models.EmailField(max_length=100,null=False,blank=False,verbose_name="ایمیل ")
+    Passwoord_user_Admin=models.CharField(max_length=50,verbose_name="پسورد ")
+    Register_date_Admin= models.DateTimeField("date published",)
   
  
  
@@ -37,23 +45,32 @@ class login_Admin(models.Model):
 ######################################### PRICE ALIBABA ##########################################
    
 class Price_Alibaba(models.Model):
-    Price=models.DecimalField(max_digits=10,decimal_places=1,null=False,blank=False)                                  # ghymat mahsool
-    Price_discount=models.DecimalField(max_digits=10,decimal_places=1,null=True,blank=True)                         # ghymat takhfif mahsool
+    class Meta: 
+        verbose_name="ورود ادمین "  
+        verbose_name_plural="ورود ادمین" 
+    Price=models.DecimalField(max_digits=10,decimal_places=1,null=False,blank=False,verbose_name="قیمت " )                                  # ghymat mahsool
+    Price_discount=models.DecimalField(max_digits=10,decimal_places=1,null=True,blank=True,verbose_name="قیمت تخفیف ")                         # ghymat takhfif mahsool
 
     def __str__(self) -> str:
         return '%s - %s'% (self.Price,self.Price_discount)
     
 ######################################## CATEGORY ##########################################
 
-class Category(models.Model):                                            
-    Name=models.CharField(max_length=100)
+class Category(models.Model): 
+    class Meta: 
+        verbose_name="دسته ها "  
+        verbose_name_plural="دسته ها"                                            
+    Name=models.CharField(max_length=100,verbose_name=" نام دسته")
     
     def __str__(self) -> str:
         return '%s'%(self.Name)
     
 ######################################### ATREBIUTE ##########################################
 class Attribute(models.Model):
-    Name=models.CharField(max_length=100,null=False,blank=False)            
+    class Meta: 
+        verbose_name="جزئیات"  
+        verbose_name_plural="جزئیات "
+    Name=models.CharField(max_length=100,null=False,blank=False,verbose_name="نام  ")            
 
     def __str__(self) -> str:
         return "{}".format(self.Name)
@@ -61,9 +78,12 @@ class Attribute(models.Model):
 
 
 ####################################### MODEL CATEGORY/ATTRIBUTE ######################################
-class CategoryAttribute(models.Model):                                                                         #kodam dasteh che vizhegihai darad
-    CategoryId=models.ForeignKey("Category",on_delete=models.CASCADE,related_name='Attribute',null=True,blank=True)
-    AttributeId=models.ForeignKey("Attribute",on_delete=models.CASCADE)
+class CategoryAttribute(models.Model):  
+    class Meta: 
+        verbose_name="جزئیات/دسته"  
+        verbose_name_plural="جزئیات/دسته "                                                 
+    CategoryId=models.ForeignKey("Category",on_delete=models.CASCADE,related_name='Attribute',null=True,blank=True,verbose_name="نام دسته  ")
+    AttributeId=models.ForeignKey("Attribute",on_delete=models.CASCADE,verbose_name="نام جزئیات")
 
     def __str__(self):
         return f'{self.CategoryId.Name}-{self.AttributeId.Name}'
@@ -71,11 +91,14 @@ class CategoryAttribute(models.Model):                                          
 ####################################### TRIP (safarha) ###################################################
 
 class Trip(models.Model):
-    TripId=models.ForeignKey("Category",on_delete=models.CASCADE)
-    TripAttribute=models.ForeignKey("TripAttribute",on_delete=models.CASCADE)
-    Attribute=models.ForeignKey(Attribute,on_delete=models.CASCADE)
-    Name=models.CharField(max_length=100) 
-    Poster=models.ImageField(upload_to="TripImages/",null=True)    # zamani k taraf bekhad ye ax estefadeh koneh upload_to= zamani ke nemikhahim hajm ax hai ke darim fazay ma ra ziad eshghal nakoneh ma yek file ya poosheh ijad mikonim va sar in masir faghat moshakhas mikonim ke az anja bekhanad.
+    class Meta: 
+        verbose_name="سفر"  
+        verbose_name_plural="سفر" 
+    TripId=models.ForeignKey("Category",on_delete=models.CASCADE,verbose_name="دسته")
+    TripAttribute=models.ForeignKey("TripAttribute",on_delete=models.CASCADE,verbose_name=" جزئیات سفر")
+    Attribute=models.ForeignKey(Attribute,on_delete=models.CASCADE,verbose_name="جزئیات")
+    Name=models.CharField(max_length=100,verbose_name=" نام") 
+    Poster=models.ImageField(upload_to="TripImages/",null=True,verbose_name="پوستر")    # zamani k taraf bekhad ye ax estefadeh koneh upload_to= zamani ke nemikhahim hajm ax hai ke darim fazay ma ra ziad eshghal nakoneh ma yek file ya poosheh ijad mikonim va sar in masir faghat moshakhas mikonim ke az anja bekhanad.
 
  
 
@@ -84,10 +107,13 @@ class Trip(models.Model):
 
 #######################################  PRODUC/ATTRIBUTE (marboot be safar) ########################################
  
-class TripAttribute(models.Model):                                                                        # vizhegi mahsoolato negahdari mikoneh
-    TripId=models.ForeignKey("Trip",on_delete=models.CASCADE)
-    AttributeId=models.ForeignKey("Attribute",on_delete=models.CASCADE)
-    Value=models.CharField(max_length=255)
+class TripAttribute(models.Model):
+    class Meta: 
+        verbose_name="جزئیات سفر"  
+        verbose_name_plural="جزئیات سفر"                                                                         # vizhegi mahsoolato negahdari mikoneh
+    TripId=models.ForeignKey("Trip",on_delete=models.CASCADE,verbose_name="سفر")
+    AttributeId=models.ForeignKey("Attribute",on_delete=models.CASCADE,verbose_name="جزئیات")
+    Value=models.CharField(max_length=255,verbose_name="مقدار")
 
     def __str__(self) -> str:
         return f'{self.TripId.Name}-{self.AttributeId.Name}'
@@ -95,10 +121,13 @@ class TripAttribute(models.Model):                                              
 ##################################### PASSENGER ############################################################
                                                                                                              # atelaat marboot be mosaferan
 class Passenger(models.Model):
-    FirstName=models.CharField(max_length=100)
-    LastName=models.CharField(max_length=100)
-    Email=models.EmailField()
-    phone=models.CharField(max_length=11,null=False,blank=False)
+    class Meta: 
+        verbose_name="مسافر"  
+        verbose_name_plural="مسافر" 
+    FirstName=models.CharField(max_length=100,verbose_name="نام")
+    LastName=models.CharField(max_length=100,verbose_name="نام خانوادگی")
+    Email=models.EmailField(verbose_name="ایمیل")
+    phone=models.CharField(max_length=11,null=False,blank=False,verbose_name="تلفن")
 
     def __str__(self) -> str:
         return '%s %s %s %s'%(self.FirstName,self.LastName,self.Email,self.phone)
@@ -106,10 +135,13 @@ class Passenger(models.Model):
 ##################################### TIKET ############################################################
                                                                                                              #tedad blit hay har shakhs va etelaat marboot be blit negah midarim.
 class Tiket(models.Model):
-    Passenger=models.ForeignKey("Passenger",on_delete=models.CASCADE)
-    TripId=models.ForeignKey("Trip",on_delete=models.CASCADE)
-    SeatNumber=models.CharField(max_length=10,null=True,blank=True)
-    quantity=models.PositiveIntegerField()                                                                    #tedad blit
+    class Meta: 
+        verbose_name="بلیط"  
+        verbose_name_plural="بلیط" 
+    Passenger=models.ForeignKey("Passenger",on_delete=models.CASCADE,verbose_name="نام مسافر")
+    TripId=models.ForeignKey("Trip",on_delete=models.CASCADE,verbose_name="سفر")
+    SeatNumber=models.CharField(max_length=10,null=True,blank=True,verbose_name="شماره صندلی")
+    quantity=models.PositiveIntegerField(verbose_name="تعداد")                                                                    #tedad blit
 
 
 
@@ -120,13 +152,16 @@ class Tiket(models.Model):
 ############################################# FACTOR #######################################################
                                                                                                                      # factor marboot be mosaferan va ghymat kol negah midarim
 class Factor(models.Model):
-    Passenger=models.ForeignKey("Passenger",on_delete=models.CASCADE)
-    TripAttribute=models.ForeignKey("TripAttribute",on_delete=models.CASCADE)
-    TotalPraice=models.DecimalField(max_digits=10,decimal_places=2)
+    class Meta: 
+        verbose_name="فاکتور"  
+        verbose_name_plural="فاکتور" 
+    Passenger=models.ForeignKey("Passenger",on_delete=models.CASCADE,verbose_name="نام مسافر")
+    TripAttribute=models.ForeignKey("TripAttribute",on_delete=models.CASCADE,verbose_name="جزئیات سفر")
+    TotalPraice=models.DecimalField(max_digits=10,decimal_places=2,verbose_name="قیمت")
     Created_at= models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
-        return f'{self.Passenger}-{self.TOtalPraice}-{self.Created_at}'
+        return f'{self.Passenger}-{self.TotalPraice}-{self.Created_at}'
     
 
 
